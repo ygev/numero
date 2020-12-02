d2cf63f7e6d73fb9a638d702372e439584d26fca
Yana <yanacommit@wizwar.net>
2020-12-01 22:55:52 -0800

    Made for loop for renaming frames in batch.

--- code.ts
+++ code.ts
@@ -1,18 +1,19 @@
-// This plugin creates 5 rectangles on the screen.
-const numberOfRectangles = 5
-
 // This file holds the main code for the plugins. It has access to the *document*.
 // You can access browser APIs such as the network by creating a UI which contains
 // a full browser environment (see documentation).
 
 const nodes: SceneNode[] = [];
-for (let i = 0; i < numberOfRectangles; i++) {
-  const rect = figma.createRectangle();
-  rect.x = i * 150;
-  rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-  figma.currentPage.appendChild(rect);
-  nodes.push(rect);
+
+const colors = figma.currentPage.findAll(n => n.name === "1" && n.type === "FRAME")
+
+console.log(colors[0].name);
+function rename(){
+  for (let i = 0; i < colors.length; i++) {
+  colors[i].name = "One";
+  }
 }
+rename();
+console.log(colors[0].name);
 figma.currentPage.selection = nodes;
 figma.viewport.scrollAndZoomIntoView(nodes);
 
